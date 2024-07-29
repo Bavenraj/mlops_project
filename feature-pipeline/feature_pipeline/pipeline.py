f567fda81464baa0cb2439e2d741bf8012283274
Bavenraj <bavenraj08@gmail.com>
2024-07-29 10:38:22 +0800

    mlops

--- feature-pipeline/feature_pipeline/pipeline.py
+++ feature-pipeline/feature_pipeline/pipeline.py
@@ -4,6 +4,7 @@ from typing import Optional
 from pathlib import Path
 import fire
 import pandas as pd
+from pandas.errors import EmptyDataError
 from yarl import URL
 import requests
 logging.basicConfig(level=logging.INFO)
@@ -36,6 +37,45 @@ def run (
             export_start=min_export_start
             export_end=export_start+datetime.timedelta(days=days_export)
             logging.warning("We clapped 'export_start' to 'datetime(2020, 6, 30, 22, 0, 0)' and 'export_end' to 'export_start + datetime.timedelta(days=days_export)' as this is the latest window available in the dataset.")
+    
+    # _extract_records_from_file_url
+    if cache_dir is None:
+        cache_dir = "./output/data"
+        cache_dir.mkdir(parents=True, exist_ok=True)
+        
+    file_path = cache_dir / "ConsumptionDE35Hour.csv"
+    if not file_path.exists():
+        logging.info(f"Downloading data from: {url}")
+
+        try:
+            response = requests.get(url)
+        except requests.exceptions.HTTPError as e:
+            logging.error(
+                f"Response status = {response.status_code}. Could not download the file due to: {e}"
+            )
+
+            return None
+        
+        if response.status_code != 200:
+            raise ValueError(f"Response status = {response.status_code}. Could not download the file.")
+    
+        with file_path.open("w") as f:
+            f.write(response.text)
+
+        logging.info(f"Successfully downloaded data to: {file_path}")
+    else:
+        logging.info(f"Data already downloaded at: {file_path}")
+
+    try:
+        data = pd.read_csv(file_path, delimiter=";")
+    except EmptyDataError:
+        file_path.unlink(missing_ok=True)
+        
+        raise ValueError(f"Downloaded file at {file_path} is empty. Could not load it into a DataFrame.")
+
+    records = data[(data["HourUTC"] >= export_start.strftime(datetime_format)) & (data["HourUTC"] < export_end.strftime(datetime_format))]
+
+    # _extract_records_from_api_url
     query_params = {
         "offset": 0,
         "sort": "HourUTC",
@@ -52,4 +92,8 @@ def run (
         response = response.json()
     except:
         logging.error(f"Response status = {response.status_code}. Could not decode response from API")
+    
+    records = response["records"]
+    records = pd.DataFrame.from_records(records)
+
 
