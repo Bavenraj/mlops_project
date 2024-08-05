5b2513b20fcf60baf93813c343c5cb3fca7efd50
BAVENRAJ <bavenraj08@gmail.com>
2024-08-05 23:25:48 +0800

    ml

--- feature-pipeline/feature_pipeline/pipeline.py
+++ feature-pipeline/feature_pipeline/pipeline.py
@@ -2,10 +2,10 @@ import datetime
 import logging
 from typing import Optional 
 from pathlib import Path
-import fire
+#import fire
 import pandas as pd
 from pandas.errors import EmptyDataError
-from yarl import 
+from yarl import URL
 import requests
 from great_expectations.core import ExpectationSuite, ExpectationConfigurationg
 import hopsworks
