25d4724e4961ea8aa968f41bb09a79dba9a0e214
BAVENRAJ <bavenraj08@gmail.com>
2024-08-08 13:04:16 +0800

    last

--- training-pipeline/training_pipeline/hyperparameter_tuning.py
+++ training-pipeline/training_pipeline/hyperparameter_tuning.py
@@ -10,7 +10,12 @@ import hopsworks
 import os
 from dotenv import load_dotenv, dotenv_values
 import logging
+import lightgbm as lgb
 
+from sktime.forecasting.compose import make_reduction, ForecastingPipeline
+from sktime.forecasting.naive import NaiveForecaster
+from sktime.transformations.series.date import DateTimeFeatures
+from sktime.transformations.series.summarize import WindowSummarizer
 load_dotenv("../.env.default")
 logging.basicConfig(level=logging.INFO)
 
@@ -184,26 +189,7 @@ def run(
     return metadata
 
 
-def run_hyperparameter_optimization(
-    y_train: pd.DataFrame, X_train: pd.DataFrame, fh: int
-):
-    """Runs hyperparameter optimization search using W&B sweeps."""
-
-    sweep_id = wandb.sweep(
-        sweep=gridsearch_configs.sweep_configs, project=SETTINGS["WANDB_PROJECT"]
-    )
-
-    wandb.agent(
-        project=SETTINGS["WANDB_PROJECT"],
-        sweep_id=sweep_id,
-        function=partial(run_sweep, y_train=y_train, X_train=X_train, fh=fh),
-    )
-
-    return sweep_id
-
-
 def run_sweep(y_train: pd.DataFrame, X_train: pd.DataFrame, fh: int):
-    """Runs a single hyperparameter optimization step (train + CV eval) using W&B sweeps."""
 
     with init_wandb_run(
         name="experiment", job_type="hpo", group="train", add_timestamp_to_name=True
@@ -212,7 +198,48 @@ def run_sweep(y_train: pd.DataFrame, X_train: pd.DataFrame, fh: int):
 
         config = wandb.config
         config = dict(config)
-        model = build_model(config)
+        lag = config.pop(
+        "forecaster_transformers__window_summarizer__lag_feature__lag",
+        list(range(1, 72 + 1)),
+        )
+        mean = config.pop(
+            "forecaster_transformers__window_summarizer__lag_feature__mean",
+            [[1, 24], [1, 48], [1, 72]],
+        )
+        std = config.pop(
+            "forecaster_transformers__window_summarizer__lag_feature__std",
+            [[1, 24], [1, 48], [1, 72]],
+        )
+        n_jobs = config.pop("forecaster_transformers__window_summarizer__n_jobs", 1)
+        window_summarizer = WindowSummarizer(
+            **{"lag_feature": {"lag": lag, "mean": mean, "std": std}},
+            n_jobs=n_jobs,
+        )
+
+        regressor = lgb.LGBMRegressor()
+        forecaster = make_reduction(
+            regressor,
+            transformers=[window_summarizer],
+            strategy="recursive",
+            pooling="global",
+            window_length=None,
+        )
+
+        pipe = ForecastingPipeline(
+            steps=[
+                ("attach_area_and_consumer_type", transformers.AttachAreaConsumerType()),
+                (
+                    "daily_season",
+                    DateTimeFeatures(
+                        manual_selection=["day_of_week", "hour_of_day"],
+                        keep_original_columns=True,
+                    ),
+                ),
+                ("forecaster", forecaster),
+            ]
+        )
+        model = pipe.set_params(**config)
+        #model = build_model(config)
 
         model, results = train_model_cv(model, y_train, X_train, fh=fh)
         wandb.log(results)
